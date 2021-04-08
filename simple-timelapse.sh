52804d1f4cead65497060a1c91c5842c3237026a
Jan Häusler <jan.haeusler@rwth-aachen.de>
2021-04-08 21:16:28 +0200

    Create simple-timelapse.sh

---
+++ simple-timelapse.sh
@@ -0,0 +1,15 @@
+cam="GENERAL WEBCAM"
+
+mkdir images
+cd images
+for (( ; ; ))
+do
+    date=$(date +%s)
+    ffmpeg -f dshow -i "video=$cam" -frames:v 30 "img-$date-%d.jpg"
+    for i in {1..29}
+    do
+        rm -f "img-$date-$i.jpg"
+    done
+    sleep $1
+done
+cd ..
